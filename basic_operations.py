4afecb38cc76c0b825e728f82ad9957686600c50
JudyXiao0804 <40845886+JudyXiao0804@users.noreply.github.com>
2018-10-04 23:23:10 +0900

    Create basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+arr = np.array([[1.0, 2.0, 3.0], [4., 5., 6.]])
+print arr * arr
+print arr - arr
+print
+
+arr = np.array([[1.0, 2.0, 3.0], [4., 5., 6.]])
+print 1 / arr
+print arr ** 0.5 
