a68d68cca5a7567d8c2fc0fa5a10736af715d8d4
JudyXiao0804 <40845886+JudyXiao0804@users.noreply.github.com>
2018-10-04 12:17:22 +0900

    Create linear_linalg.py

---
+++ linear_linalg.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import numpy.random as np_random
+from numpy.linalg import inv, qr
+
+x = np.array([[1., 2., 3.], [4., 5., 6.]])
+y = np.array([[6., 23.], [-1, 7], [8, 9]])
+print x.dot(y)
+print np.dot(x, np.ones(3))
+x = np_random.randn(5, 5)
+print
+
+
+mat = x.T.dot(x)
+print inv(mat)  # matrix T
+print mat.dot(inv(mat)) # 与逆矩阵相乘，得到单位矩阵。
+print
+
+print '矩阵消元'
+print mat
+q, r = qr(mat)
+print q
+print r
